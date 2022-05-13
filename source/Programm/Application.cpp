1d28134f5d1677a3e63a88ed579b744f9ddad5ce
Ronti <nasowasaberauch@posteo.de>
2022-05-13 16:38:27 +0200

    use simpler example code

--- source/Programm/Application.cpp
+++ source/Programm/Application.cpp
@@ -7,35 +7,17 @@
 
 void Application::onRun()
 {
-    auto* obj_a = new ClassA;
-    auto* obj_b = new ClassB;
-    auto* obj_c = new ClassC;
-    auto* obj_d = new ClassD;
+    const auto obj_c = ClassC();
 
-    connect(this, &Application::goDown, obj_a, &ClassA::handleGoDown);
-    connect(this, &Application::goDown, obj_b, &ClassB::handleGoDown);
-    connect(this, &Application::goDown, obj_c, &ClassC::handleGoDown);
-    connect(this, &Application::goDown, obj_d, &ClassD::handleGoDown);
+    connect(this, &Application::goDown, &obj_c, &ClassC::handleGoDown);
+    connect(&obj_c, &ClassC::goUp, this, &Application::handleGoUp);
     
-    connect(obj_a, &ClassA::goUp, this, &Application::handleGoUp);
-    connect(obj_b, &ClassB::goUp, this, &Application::handleGoUp);
-    connect(obj_c, &ClassC::goUp, this, &Application::handleGoUp);
-    connect(obj_d, &ClassD::goUp, this, &Application::handleGoUp);
-
     emit goDown(QString(__FUNCTION__) + "\n");
-
-    delete obj_a;
-    delete obj_b;
-    delete obj_c;
-    delete obj_d;
-
+    
     exit();
 }
 
 void Application::handleGoUp(QString trace)
 {
     qDebug().noquote() << (trace + __FUNCTION__ + "\n");
-
-    int ii = 0;
-    ii++;
 }
